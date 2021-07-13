fe198b18530fc0451b83c50bb11e208cc3f1404b
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-07-12 21:58:38 -0500

    Create question component

---
+++ client/src/components/Questions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Questions() {
+
+    const divStyle = {
+
+    }
+
+    return (
+        <div style={divStyle}>
+            
+        </div>
+    )
+}

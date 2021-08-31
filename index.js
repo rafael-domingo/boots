5f7da791d0832e5a80d8d47cb0ea624421caf011
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-08-31 15:09:00 -0500

    Set up for Heroku deployment

--- index.js
+++ index.js
@@ -6,35 +6,22 @@ const axios = require('axios');
 const yelp = require('./yelp');
 const maps = require('./maps');
 const port = process.env.PORT || 5000;
-
+const path = require('path')
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.use('/yelp', yelp);
 app.use('/maps', maps);
-// var google_api_key = process.env.GOOGLE_MAPS_API_KEY;
 
 
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
-
-// Google Maps Places API
-// async function makeRequest() {
-//     try {
-//         const neighborhood = 'chelsea'
-//         const borough = 'manhattan'
-//         const city = 'new+york+city'
-//         const category = 'burgers'
-//         const {data} = await axios.get(
-        
-//      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${category}+${neighborhood}+${borough}+${city}&type=restaurant&key=${google_api_key}`
-//         )
-//         console.log(data.results)
-//         } 
-//       catch (err) {
-//        console.log(err)
-//      }
-// }
-
-// makeRequest()
 app.listen(port, () => console.log(`Server started on ${port}`));
